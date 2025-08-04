41965443a2beedac28d4125e85dcecf3439cdbde
gonzalesmatias745-cmd <gonzalesmatias745@gmail.com>
2025-08-04 07:56:55 -0500

    ejercico1

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+a = float(input("Ingrese un numero: "))
+b = float(input("Ingrese otro numero: "))
+
+print(a, " + ", b, " = ", (a + b))
+print(a, " - ", b, " = ", (a - b))
+print(a, " * ", b, " = ", (a * b))  
+print(a, " / ", b, " = ", (a / b))
